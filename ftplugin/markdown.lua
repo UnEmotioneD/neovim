ab5aa03ee366480abb68e74cc1133f8d9b4f8789
UnEmotioneD <blackeagle10@icloud.com>
2025-03-30 03:29:34 +0900

    remove obsidian.nvim keymaps from ftplugin/markdown.lua

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -6,8 +6,6 @@ opt.linebreak = true
 opt.textwidth = 80
 opt.formatoptions:append('t') -- Automatically wrap text using textwidth
 
--- vim.o.conceallevel = 2 -- for obsidian.nvim
-
 -- Fold headers
 opt.foldmethod = 'expr'
 opt.foldexpr = "getline(v:lnum)=~'^\\s*#' ? '>' . (strlen(submatch(0)) - 1) : '='"
@@ -20,6 +18,4 @@ map('n', 'k', 'gk')
 
 map('n', '<leader>mp', '<Cmd>MarkdownPreview<CR>', { desc = '[m]arkdown [p]erview' })
 
-map('n', '<leader>mr', '<Cmd>RenderMarkdown toggle<CR>', { desc = '[m]arkdown [r]ender for current buffer' })
-
--- map('n', '<leader>mo', '<Cmd>ObsidianOpen<CR>', { desc = '[m]arkdown in [o]bsidian' })
+map('n', '<leader>mr', '<Cmd>RenderMarkdown toggle<CR>', { desc = '[m]arkdown [r]ender' })
