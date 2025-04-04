c20bdb6a142308fe3f14450c5345eb617bee5f5d
mac <blackeagle10@icloud.com>
2025-04-04 14:06:12 +0900

    html.lua snippet 에 주석, meta 태그 추가

--- snippets/html.lua
+++ snippets/html.lua
@@ -3,6 +3,9 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
+-- 느낌표(!, bang) 를 입력하고 자동완성에서 선택하면 아래와 같은 코드를 완성 시키고
+--  <Tab>을 눌러서 커서를 i(1) 위치에서 i(0)으로 이동 할 수 있다
+--  (prettier 로 포멧 했을때의 형식)
 return {
   s('!', {
     t('<!doctype html>'),
@@ -13,13 +16,15 @@ return {
     t({ '', '    <title>' }),
     i(1),
     t('</title>'),
-    t({ '', '    <link rel="stylesheet" href="" />' }),
     t({ '', '    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>' }),
+    t({ '', '    <link rel="stylesheet" href="style.css" />' }),
+    t({ '', '    <style></style>' }),
     t({ '', '  </head>' }),
     t({ '', '  <body>' }),
     t({ '', '    ' }),
     i(0),
     t({ '', '  </body>' }),
+    t({ '', '  <scripts> </scripts>' }),
     t({ '', '</html>' }),
   }),
 }
