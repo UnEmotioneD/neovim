baebaf7f451418e48d84af73cdcef72ea83bcddf
mac <blackeagle10@icloud.com>
2025-04-09 22:05:17 +0900

    Update(telescope) : add telescope-ui as dependency && load extension correctrly

--- lua/unemotioned/plugins/telescope.lua
+++ lua/unemotioned/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
   'nvim-telescope/telescope.nvim',
   dependencies = {
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    'nvim-telescope/telescope-ui-select.nvim',
   },
   config = function()
     local telescope = require('telescope')
@@ -12,27 +13,39 @@ return {
         path_display = { 'smart' },
 
         layout_config = {
-          ---@type string | 'bottom' | 'top'
-          prompt_position = 'bottom',
+          prompt_position = 'bottom', ---@type string | 'bottom' | 'top'
         },
-        ---@type string | 'horizontal' | 'vertical'
-        layout_strategy = 'horizontal',
-        ---@type string | 'ascending' | 'descending'
-        sorting_strategy = 'descending',
+        layout_strategy = 'horizontal', ---@type string | 'horizontal' | 'vertical'
+        sorting_strategy = 'descending', ---@type string | 'ascending' | 'descending'
 
         mappings = {
           i = {
-            ['<C-k>'] = actions.move_selection_previous,
             ['<C-j>'] = actions.move_selection_next,
+            ['<C-k>'] = actions.move_selection_previous,
             ['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
+            ['<C-w>'] = actions.which_key,
+            ['<C-x>'] = nil, -- disable default keymap to open in horizontal split
+            ['<C-h>'] = actions.select_horizontal,
           },
         },
       },
+
+      pickers = {
+        find_files = {
+          hidden = false,
+        },
+      },
       extensions = {
-        fzf = {},
+        fzf = {
+          fuzzy = true,
+        },
+        ['ui-select'] = {
+          require('telescope.themes').get_dropdown(),
+        },
       },
     })
 
     telescope.load_extension('fzf')
+    telescope.load_extension('ui-select')
   end,
 }
