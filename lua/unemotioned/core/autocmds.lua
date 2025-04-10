78955225a26237b5e15cdbacb8634cf634a82139
arch <blackeagle10@icloud.com>
2025-04-11 08:09:12 +0900

    Update(autocmd) : highlight -> hl

--- lua/unemotioned/core/autocmds.lua
+++ lua/unemotioned/core/autocmds.lua
@@ -2,7 +2,7 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('HighlightYank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 300 })
+    vim.hl.on_yank({ higroup = 'IncSearch', timeout = 300 })
   end,
 })
 
