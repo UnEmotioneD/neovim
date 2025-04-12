61bf740609cf6a0c7171f5effd2c0da6a7758d9b
mac <blackeagle10@icloud.com>
2025-04-13 08:12:57 +0900

    Update(lualine) : load default settings

--- lua/unemotioned/plugins/lualine.lua
+++ lua/unemotioned/plugins/lualine.lua
@@ -6,6 +6,11 @@ return {
       theme = 'auto',
       component_separators = { left = '', right = '' },
       section_separators = { left = '', right = '' },
+      disabled_filetypes = {
+        statusline = {},
+        winbar = {},
+      },
+      ignore_focus = {},
       always_divide_middle = true,
       always_show_tabline = true,
       globalstatus = false,
@@ -17,9 +22,9 @@ return {
     },
     sections = {
       lualine_a = { 'mode' },
-      lualine_b = { 'branch' },
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
       lualine_c = { 'filename' },
-      lualine_x = { 'filesize' },
+      lualine_x = { 'encoding', 'fileformat', 'filetype' },
       lualine_y = { 'progress' },
       lualine_z = { 'location' },
     },
@@ -31,5 +36,9 @@ return {
       lualine_y = {},
       lualine_z = {},
     },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {},
   },
 }
