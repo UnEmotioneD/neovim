8eaa6900ba2d464fc1ad2291f951c94939cb696d
mac <blackeagle10@icloud.com>
2025-04-14 13:03:13 +0900

    Fix(dashboard) : session restore command

--- lua/unemotioned/plugins/dashboard.lua
+++ lua/unemotioned/plugins/dashboard.lua
@@ -14,7 +14,7 @@ return {
           { desc = ' Explorer', group = 'Type', action = 'NvimTreeToggle', key = 'e' },
           { desc = '󰱼 Files', group = 'Type', action = 'Telescope find_files', key = 'f' },
           { desc = ' Words', group = 'Type', action = 'Telescope live_grep', key = 'w' },
-          { desc = '󰁯 Restore', group = 'Type', action = 'RestoreSession', key = 'r' },
+          { desc = '󰁯 Restore', group = 'Type', action = 'SessionRestore', key = 'r' },
           { desc = ' Git', group = '@property', action = 'LazyGit', key = 'g' },
           { desc = '󰩈 Quit', group = 'Boolean', action = 'wqa', key = 'q' },
         },
