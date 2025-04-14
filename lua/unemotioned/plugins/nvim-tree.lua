431f729580b8e3f98eef1db5812fd3fd7e6a5f0a
mac <blackeagle10@icloud.com>
2025-04-15 08:26:21 +0900

    Refactor(nvim-tree) : move options from config to opts block

--- lua/unemotioned/plugins/nvim-tree.lua
+++ lua/unemotioned/plugins/nvim-tree.lua
@@ -1,41 +1,38 @@
 return {
   'nvim-tree/nvim-tree.lua',
   dependencies = 'stevearc/dressing.nvim',
-  config = function()
-    require('nvim-tree').setup({
-      view = {
-        ---@type 'left' | 'right'
-        side = 'left',
-        relativenumber = true,
-        width = 40,
+  opts = {
+    view = {
+      side = 'left', ---@type 'left' | 'right'
+      relativenumber = true,
+      width = 40,
+    },
+    renderer = {
+      indent_markers = {
+        enable = true,
+        inline_arrows = true,
       },
-      renderer = {
-        indent_markers = {
-          enable = true,
-          inline_arrows = true,
-        },
-        icons = {
-          glyphs = {
-            folder = {
-              arrow_closed = '',
-              arrow_open = '',
-            },
+      icons = {
+        glyphs = {
+          folder = {
+            arrow_closed = '',
+            arrow_open = '',
           },
         },
       },
-      git = {
-        enable = false,
-      },
-      diagnostics = {
-        enable = false,
-      },
-      modified = {
-        enable = false,
-      },
-      filters = {
-        enable = true,
-        custom = { '.DS_Store' },
-      },
-    })
-  end,
+    },
+    git = {
+      enable = false,
+    },
+    diagnostics = {
+      enable = false,
+    },
+    modified = {
+      enable = false,
+    },
+    filters = {
+      enable = true,
+      custom = { '.DS_Store' },
+    },
+  },
 }
