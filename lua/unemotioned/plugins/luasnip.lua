f6afefa7960ed054c5616843451fa43f28678b08
mac <blackeagle10@icloud.com>
2025-05-03 22:37:18 +0900

    Fix(luasnip): path to snippets nvim -> daniel

--- lua/unemotioned/plugins/luasnip.lua
+++ lua/unemotioned/plugins/luasnip.lua
@@ -32,7 +32,7 @@ return {
     })
 
     require('luasnip.loaders.from_lua').lazy_load({
-      paths = { '~/.config/nvim/snippets' },
+      paths = { '~/.config/daniel/snippets' },
     })
   end,
 }
