739d7e0fe1953ea9c8301e9c2f039ca32de48c2a
mac <blackeagle10@icloud.com>
2025-05-05 23:35:51 +0900

    Chore(fidget): clean up config file

--- lua/unemotioned/plugins/fidget.lua
+++ lua/unemotioned/plugins/fidget.lua
@@ -2,15 +2,9 @@ return {
   'j-hui/fidget.nvim',
   opts = {
     progress = {
-      poll_rate = 0, -- How and when to poll for progress messages
-      suppress_on_insert = false,
+      suppress_on_insert = true,
       ignore_done_already = true,
       ignore_empty_message = true,
-      ignore = {},
-      lsp = {
-        progress_ringbuf_size = 0, -- Configure the nvim's LSP progress ring buffer size
-        log_handler = false, -- Log `$/progress` handler invocations (for debugging)
-      },
     },
     integration = {
       ['nvim-tree'] = {
