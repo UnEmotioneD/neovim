683c04ede0f9e938720f1872c00b0feb1ab6f5f5
mac <blackeagle10@icloud.com>
2025-05-06 00:00:48 +0900

    Fix(tabout): disble luasnip in tabout

--- lua/unemotioned/plugins/tabout.lua
+++ lua/unemotioned/plugins/tabout.lua
@@ -1,7 +1,16 @@
 return {
-  'abecodes/tabout.nvim',
-  lazy = false,
-  opts = {},
-  priority = 1000,
-  event = 'InsertCharPre',
+  {
+    'abecodes/tabout.nvim',
+    lazy = false,
+    opts = {},
+    priority = 1000,
+    event = 'InsertCharPre',
+  },
+  {
+    'L3MON4D3/LuaSnip',
+    keys = function()
+      -- Disable default tab keybinding in LuaSnip
+      return {}
+    end,
+  },
 }
