bc3e34ec952655005f0e4dfd9a9925ffbe6df2ac
mac <blackeagle10@icloud.com>
2025-05-05 23:37:58 +0900

    Minor changes in comment.nvim config file

--- lua/unemotioned/plugins/comment.lua
+++ lua/unemotioned/plugins/comment.lua
@@ -2,9 +2,8 @@ return {
   'numToStr/Comment.nvim',
   dependencies = 'JoosepAlviste/nvim-ts-context-commentstring',
   config = function()
+    -- For commenting html, jsx, tsx, svelte files and etc ...
     local ts_context_commentstring = require('ts_context_commentstring.integrations.comment_nvim')
-    -- for commenting html, jsx, tsx, svelte files
-
     require('Comment').setup({
       pre_hook = ts_context_commentstring.create_pre_hook(),
     })
