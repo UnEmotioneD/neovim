7b0efcf78c0cdf3628fac00b0449bb5240807f58
mac <blackeagle10@icloud.com>
2025-05-09 23:21:37 +0900

    Update(quick-scope): add event and toggle keymap

--- lua/unemotioned/plugins/quick-scope.lua
+++ lua/unemotioned/plugins/quick-scope.lua
@@ -1,3 +1,7 @@
 return {
   'unblevable/quick-scope',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    vim.keymap.set('n', '<leader>tq', ':QuickScopeToggle<CR>', { desc = 'Toggle quick-scope' })
+  end,
 }
